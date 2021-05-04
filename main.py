0030e4fc37e80d0519ee956dd447646aeceaa8fc
audrey.ly <audrey.ly@u-psud.fr>
2021-05-04 12:08:47 +0200

    working minimax

--- main.py
+++ main.py
@@ -2,26 +2,41 @@
 from math import inf as infinity
 import copy
 
-board = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-]
-dict_etat = {}
-dict_lien = {}
-dict_score = {}
 COMP = 1
 HUMAN = -1
 
-def empty_cells(state):
-    cells = []
+def children_states(state):
+    turn = HUMAN if sum(state) == 0 else COMP
+    children = []
+    for i in range(9):
+        if state[i] == 0:
+            children +=  [tuple(state[j]  if j!=i else turn for j in range(9))]
+    return children
 
-    for x, row in enumerate(state):
-        for y, cell in enumerate(row):
-            if cell == 0:
-                cells.append([x, y])
 
-    return cells
+Three_aligned = {(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)}
+
+def eval_win(state):
+    for (i,j,k) in Three_aligned:
+        if state[i] != 0 and state[i] == state[j] and state[j] == state[k]:
+            return state[i]
+    for i in range(9):
+        if state[i] == 0:
+            return children_states(state)
+    return 0
+
+Start = tuple(0 for i in range(9))
+Tree = {}
+Scores = {}
+
+def fill_tree(current_state):
+    children = eval_win(current_state)
+    if type(children) == list:
+        for child_state in children:
+            if child_state not in Tree:
+                fill_tree(child_state)
+    Tree[current_state] = children
+
 
 def score(state):
     if wins(state, COMP):
@@ -37,122 +52,61 @@ def score(state):
     return score
 
 def human_turn():
+    global Start
     move = -1
-    moves = {
-        1: [0, 0], 2: [0, 1], 3: [0, 2],
-        4: [1, 0], 5: [1, 1], 6: [1, 2],
-        7: [2, 0], 8: [2, 1], 9: [2, 2],
-    }
     print(f'Human turn ["O"]')
-    #print_board(board)
+    print_board()
 
     while move < 1 or move > 9:
         try:
             move = int(input('Use numpad (1..9): '))
-            coord = moves[move]
-            can_move = set_move(coord[0], coord[1], HUMAN)
+            can_move = (Start[move-1] == 0)
 
             if not can_move:
                 print('Bad move')
                 move = -1
+            else:
+                Start = tuple(Start[j]  if j!=move-1 else -1 for j in range(9))
         except (EOFError, KeyboardInterrupt):
             print('error')
             exit()
         except (KeyError, ValueError):
             print('Bad choice')
 
-def init_tree():
-    
-    dict_etat[0] = [ [0,0,0], [0,0,0], [0,0,0] ]
-    dict_lien[0] = []
-    minimax(HUMAN, 0)
 
-def choose_move(state):
-    print("state")
-    print(state)
-    print("********")
-    ind = [key  for (key, value) in dict_etat.items() if value == state][0]
-    print(ind)
-    children = dict_lien.get(ind)
+def choose_move():
+    global Start,Scores
+    children = eval_win(Start)
     best = -infinity
-    best_index = 0
+    best_index = None
     for c in children:
-        print(dict_etat.get(c))
-        if dict_score.get(c) > best:
-            best = dict_score.get(c)
+        if Scores[c] > best:
+            best = Scores.get(c)
             best_index = c
-
-    return dict_etat.get(best_index)
+    Start = best_index;
 
 
 def ai_turn(): 
     global board
-    move = 1
-    moves = {
-        1: [0, 0], 2: [0, 1], 3: [0, 2],
-        4: [1, 0], 5: [1, 1], 6: [1, 2],
-        7: [2, 0], 8: [2, 1], 9: [2, 2],
-    }
     print(f'AI turn ["X"]')
-    print_board(board)
-    board = choose_move(board)
-    print_board(board)
-    #x, y, z =  minimax(board, len(empty_cells(board)), COMP)
-    #set_move(x, y, COMP)
+    print_board()
+    choose_move()
 
-def minimax(player, netat):
-    global dict_etat,dict_lien,dict_score
-    if netat == 1436:
-        print(dict_etat.get(netat))
 
-    d = len(empty_cells(dict_etat[netat]))
-
-    if player == COMP:
-        best = -infinity
+def minimax(current_state):
+    global Scores
+    children = eval_win(current_state)
+    if type(children) == int:
+        score = children
     else:
-        best = +infinity
-
-    if d == 0 or game_over(dict_etat[netat]):
-        #print("cas limite")
-        dict_score[netat] = score(dict_etat[netat])
-        return 
-    
-    for cell in empty_cells(dict_etat[netat]):
-        x, y = cell[0], cell[1]
-        #print(dict_etat)
-        tmp = copy.deepcopy(dict_etat[netat])
-        #tmp = tmp.copy()
-        tmp[x][y] = player
-
-        if netat ==1436:
-            print(tmp)
-        #print(dict_etat)
-        child = None
-
-        if tmp in dict_etat.values():
-            child = [key  for (key, value) in dict_etat.items() if value == tmp]
-            child = child[0]
-            dict_lien[netat].append(child)
-            #print("in values")
-            #return
-        else:
-            child = len(dict_etat)
-            dict_etat[child] = tmp
-            dict_lien[netat].append(child)
-            dict_lien[child] = []
-            #print("avant minimax recursion")
-            minimax(-player,child)
-
-        
-
-        if player == COMP:
-            if dict_score.get(child) > best:
-                best = dict_score.get(child)  # max value
-        else:
-            if dict_score.get(child) < best:
-                best = dict_score.get(child)  # min value
-
-    dict_score[netat] = best
+        turn = HUMAN if sum(current_state) == 0 else COMP
+        score = -turn
+        for child_state in children:
+            if child_state not in Tree:
+                minimax(child_state)
+            score = max(score,Scores[child_state]) if turn == COMP else min(score,Scores[child_state])
+    Tree[current_state] = children
+    Scores[current_state] = score
 
 
 def valid_move(x, y):
@@ -162,22 +116,15 @@ def valid_move(x, y):
         return False
 
 
-def set_move(x, y, player):
-    global board
-    if valid_move(x, y):
-        board[x][y] = player
-        return True
-    else:
-        return False
-
-def print_board(state):
+def print_board():
+    global Start
     print("*************")
-    for x, row in enumerate(state):
+    for x in range(0,3):
         s = "|"
-        for y, cell in enumerate(row):
-            if state[x][y] == 0:
+        for y in range(0,3):
+            if Start[x*3+y] == 0:
                 s+="   "
-            elif state[x][y] == COMP :
+            elif Start[x*3+y] == COMP :
                 s+=" X "
             else:
                 s+=" O "
@@ -186,44 +133,26 @@ def print_board(state):
         print("*************")
 
 
-
-# player can be human or AI
-def wins(state, player):
-    
-    board_state = [
-        [state[0][0], state[0][1], state[0][2]],
-        [state[1][0], state[1][1], state[1][2]],
-        [state[2][0], state[2][1], state[2][2]],
-        [state[0][0], state[1][0], state[2][0]],
-        [state[0][1], state[1][1], state[2][1]],
-        [state[0][2], state[1][2], state[2][2]],
-        [state[0][0], state[1][1], state[2][2]],
-        [state[2][0], state[1][1], state[0][2]],
-    ]
-    if [player, player, player] in board_state:
-        return True
-    else:
-        return False
-
-def game_over(state):
-    return wins(state, HUMAN) or wins(state, COMP)
-
 def main():
 
-    #print_board(board)
-    init_tree()
+    #fill_tree(Start)
     print("********************")
-    l = dict_lien.get(1436)
-    for e in l:
-        print(dict_etat.get(e))
-    while (not game_over(board)) and (len(empty_cells(board)) != 0):
+    minimax(Start)
+    print(len(Tree))
+    print(len(Scores))
+
+    while (type(eval_win(Start)) != int):
     
         human_turn()
+        if(type(eval_win(Start)) == int):
+            break
         #todo lecture arbre faire un choix
         ai_turn()
-    if score(board) == 1:
+    print_board();
+    score = eval_win(Start)
+    if score == 1:
         print("AI win")
-    elif score(board) == -1:
+    elif score == -1:
         print("Human win")
     else:
         print("draw")
